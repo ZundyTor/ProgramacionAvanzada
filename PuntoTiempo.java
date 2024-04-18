ecf0b37ff22a671fd0a2607bcc9e47654fdcd100
DLerzundyTorres <114622303+ZundyTor@users.noreply.github.com>
2024-04-18 10:31:37 -0500

    Add files via upload

---
+++ PuntoTiempo.java
@@ -0,0 +1,26 @@
+package clasesJava;
+
+public class PuntoTiempo extends Punto{
+    private int t;
+
+    public PuntoTiempo() {
+        this(0, 0, 0);
+    }
+
+    public PuntoTiempo(int x, int y, int t) {
+        super();
+        this.setT(t);
+    }
+
+    public int getT() {
+        return this.t;
+    }
+
+    public final void setT(int t) {
+        this.t = t;
+    }
+
+    public double velocidad() {
+        return this.modulo() / this.getT();
+    }
+}
